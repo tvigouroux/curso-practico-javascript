2be4b1f41fd759cc4d77e14cae60258a5584fe08
Tomás Vigouroux <tvigouroux@gmail.com>
2022-03-06 14:25:08 -0300

    logré hacer formulario para perímetro y área de un cuadrado, el siguiente paso es agregar para triángulo y círculo

--- figuras.js
+++ figuras.js
@@ -1,14 +1,29 @@
-// Código del cuadrado
-const perimetroCuadrado = lado => lado * 4;
 
+// Cuadrado
+const perimetroCuadrado = lado => lado * 4;
 const areaCuadrado = lado => lado ** 2;
 
+// Triángulo
 const perimetroTriangulo = (lado1, lado2, base) => lado1 + lado2 + base;
-
 const areaTriangulo = (base, altura) => (base * altura) / 2;
 
+// Círculo
 const diametroCirculo = radio => radio * 2;
-
 const perimetroCirculo = radio => diametroCirculo(radio) * Math.PI;
-
 const areaCirculo = radio => Math.PI * ((diametroCirculo(radio) / 2) ** 2);
+
+// Interactuando con HTML
+
+function calcularPerimetroCuadrado() {
+    const input = document.getElementById("inputLadoCuadrado");
+    const value = input.value;
+    const perimetro = perimetroCuadrado(value);
+    alert(perimetro);
+}
+
+function calcularAreaCuadrado() {
+    const input = document.getElementById("inputLadoCuadrado");
+    const value = input.value;
+    const area = areaCuadrado(value);
+    alert(area);
+}
