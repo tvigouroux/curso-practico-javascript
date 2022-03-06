55be432a4de7a84b4e41dce74e411edfc4d49de5
Tomás Vigouroux <tvigouroux@gmail.com>
2022-03-06 17:45:18 -0300

    solucion al reto de obtener si triangulo es isoceles y se agrega formula general para calcular perímetro y area de cualquier triangulo sin importar tipo

--- figuras.js
+++ figuras.js
@@ -4,8 +4,29 @@ const perimetroCuadrado = lado => lado * 4;
 const areaCuadrado = lado => lado ** 2;
 
 // Triángulo
-const perimetroTriangulo = (lado1, lado2, base) => lado1 + lado2 + base;
-const areaTriangulo = (base, altura) => (base * altura) / 2;
+function revisarIsoceles(lado1, lado2, lado3) {
+    esIsoceles = false;
+    if (lado1 == lado2) {
+        esIsoceles = true;
+    }
+    else if (lado1 == lado3) {
+        esIsoceles = true;
+    }
+    else if (lado2 == lado3) {
+        esIsoceles = true;
+    }
+    return esIsoceles;
+}
+function hHeron(lado1, lado2, lado3) {
+    s = (lado1 + lado2 + lado3) / 2;
+    a = s * (s - lado1) * (s - lado2) * (s - lado3);
+    area = Math.sqrt(a);
+    altura = 2 * area / lado1;
+    return altura;
+}
+
+const perimetroTriangulo = (lado1, lado2, lado3) => lado1 + lado2 + lado3;
+const areaTriangulo = (lado1, lado2, lado3) => (lado1 * hHeron(lado1,lado2,lado3)) / 2;
 
 // Círculo
 const diametroCirculo = radio => radio * 2;
@@ -29,16 +50,30 @@ function calcularAreaCuadrado() {
 function calcularPerimetroTriangulo() {
     const lado1 = parseInt(document.getElementById("inputLado1Triangulo").value);
     const lado2 = parseInt(document.getElementById("inputLado2Triangulo").value);
-    const base = parseInt(document.getElementById("inputBaseTriangulo").value);
-    const perimetro = perimetroTriangulo(lado1, lado2, base);
+    const lado3 = parseInt(document.getElementById("inputLado3Triangulo").value);
+    const perimetro = perimetroTriangulo(lado1, lado2, lado3);
     alert(perimetro);
 }
 
 function calcularAreaTriangulo() {
-    const base = parseInt(document.getElementById("inputBaseTriangulo").value);
-    const altura = parseInt(document.getElementById("inputAlturaTriangulo").value);
-    const area = areaTriangulo(base, altura);
+    const lado1 = parseInt(document.getElementById("inputLado1Triangulo").value);
+    const lado2 = parseInt(document.getElementById("inputLado2Triangulo").value);
+    const lado3 = parseInt(document.getElementById("inputLado3Triangulo").value);
+    const area = areaTriangulo(lado1, lado2, lado3);
     alert(area);
+}
+
+function verificarIsoceles() {
+    const lado1 = parseInt(document.getElementById("inputLado1Triangulo").value);
+    const lado2 = parseInt(document.getElementById("inputLado2Triangulo").value);
+    const lado3 = parseInt(document.getElementById("inputLado3Triangulo").value);
+    if (revisarIsoceles(lado1, lado2, lado3)) {
+        alert("Es isóceles!!!");
+    }
+    else {
+        alert("No es isóceles :(");
+    }
+    
 }
 
 function calcularPerimetroCirculo() {
