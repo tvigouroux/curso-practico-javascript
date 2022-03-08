788d1ee777f08e19d2c0e11982a311ecaa647575
Tomás Vigouroux <tvigouroux@gmail.com>
2022-03-08 11:33:47 -0300

    reto taller 2 completado con bonus para llenar automáticamente lista con cupones disponibles

--- descuentos.js
+++ descuentos.js
@@ -1,4 +1,15 @@
+const discountCoupons = [10,15,20,30,50];
+
 const priceFinal = (priceInitial, discount) => priceInitial * (1-(discount/100));
+
+function fillSelect(list) {
+    let options = "";
+    for (const element of list) {
+        options = options + '<option value="' + element + '">' + element + '</option>';
+        console.log(options);
+    }
+    document.getElementById("InputDiscount").innerHTML = options;
+}
 
 function calculateDiscount() {
     const originalPrice = parseInt(document.getElementById("InputPrice").value);
